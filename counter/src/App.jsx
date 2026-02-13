66234ae8a48d40dd3c176ce056ddb70c56df2c7f
DenWaritthon <waritt10@gmail.com>
2026-02-13 19:22:41 +0700

    add wallpaper

---
+++ counter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Background from './component/Background'
+
+function App() {
+  return (
+    <div>
+      <Background />
+      <div className='app'> 
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ counter/src/component/Background.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Background.css'
+
+function Background() {
+  return (
+    <div className="wallpaper"></div>
+  )
+}
+
+export default Background
