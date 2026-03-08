94768a7c7822acf00bf7a0a1e217ffa8e9df5fe2
DenWaritthon <waritt10@gmail.com>
2026-03-08 12:18:44 +0700

    submit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Fun 06 Three.js
 Waritthon Kongnoo - 65340500050
 
+<video controls src="Demo.mp4" title="Demo"></video>
--- lightShow/src/Scene.jsx
+++ lightShow/src/Scene.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
-import CheckerFloor from './components/CheckerFloor';
+import Floor from './components/Floor';
 import TorusKnot from './components/TorusKnot';
-import Title3D from './components/Title3D';
-import RectLightRig from './components/RectLightRig';
+import Text from './components/Text';
+import LightEffect from './components/LightEffect';
 
 export default function Scene() {
   useEffect(() => {
@@ -16,10 +16,10 @@ export default function Scene() {
       <color attach="background" args={['#000000']} />
       <ambientLight intensity={0.02} />
 
-      <RectLightRig />
-      <CheckerFloor />
+      <LightEffect />
+      <Floor />
       <TorusKnot />
-      <Title3D />
+      <Text />
 
       <OrbitControls enableDamping target={[0, 6, 0]} />
     </>
